c9fe3974ef08a227823ccad30b574f89df56e323
Anthony Fontanez <anthony.fontanez@rit.edu>
2018-11-11 16:02:11 -0500

    Fixed indenting/whitepace

--- CreateNewADForest.ps1
+++ CreateNewADForest.ps1
@@ -1,6 +1,14 @@
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$DomainName,
+    [Parameter(Mandatory=$True)]
+    [string]$DomainNetbiosName
+)
+
 $Features = (
-	"AD-Domain-Services";
-	"DNS"
+    "AD-Domain-Services";
+    "DNS"
 )
 
 Install-WindowsFeature -Name $Features
@@ -8,17 +16,17 @@ Install-WindowsFeature -Name $Features
 Import-Module -Name ADDSDeployment
 
 $Parameters = @{
-	DomainName				= 'ad.domain.tld';
-	DomainNetbiosName		= 'AD';
-	ForestMode				= 'WinThreshold';
-	DomainMode				= 'WinThreshold';
-	DatabasePath			= 'C:\Windows\NTDS';
-	LogPath					= 'C:\Windows\NTDS';
-	SysvolPath				= 'C:\Windows\SYSVOL';
-	InstallDns				= $true;
-	CreateDnsDelegation		= $false;
-	NoRebootOnCompletion	= $false;
-	Verbose 				= $true
+    DomainName           = $DomainName;
+    DomainNetbiosName    = $DomainNetbiosName;
+    ForestMode           = 'WinThreshold';
+    DomainMode           = 'WinThreshold';
+    DatabasePath         = 'C:\Windows\NTDS';
+    LogPath              = 'C:\Windows\NTDS';
+    SysvolPath           = 'C:\Windows\SYSVOL';
+    InstallDns           = $true;
+    CreateDnsDelegation  = $false;
+    NoRebootOnCompletion = $false;
+    Verbose              = $true
 }
 
 Install-ADDSForest @Parameters
